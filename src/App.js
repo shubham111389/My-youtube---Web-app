9219285afd45548075e4465e6d3904a58caf6562
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-08-13 17:00:17 +0530

    add the header footer and body component in the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import Header  from './Components/Header'
+import Body from './Components/Body';
+import Footer from './Components/Footer';
 
+
+/* My  MYutbe structure will look like this, 
+            1) Header
+                - Logo
+                - ]Search Bar
+                - icon
+            2) Body
+                -Sidebar
+                - Body
+                   - list
+                   - videocard
+                        - video
+                        - Name
+                        -comments
+            3) Footer
+                - Links
+                - Copyrights
+       
+*/
 
 
 function App() {
   return (
-    <div className="text-lg" >
-      Hello world!
+    <div className="text-lg " >
+    
+ <Header />
+ <Footer />
+ <Body />
+  
+      hii
+      
+      
      
     </div>
   );
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>SIdebar</div>
+  )
+}
+
+export default Sidebar
