0d415bf5fee5cb9be9042f095baf568386214dab
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-08-24 11:56:39 +0530

    add the calculate date for videocard

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -1,30 +1,37 @@
 import React from "react";
 import user from "../assets/img/user.png"
 import verified from "../assets/img/verified.png"
+import { calculateDate } from "../utils/calculateDate";
+
 
 
 
 const VideoCard = ({ videoData }) => {
-  console.log(videoData);
+
   const { snippet, statistics } = videoData;
   const { channelTitle, title, thumbnails } = snippet;
   return (
-    <div className="w-72 border border-blue-300 m-2 p-2">
-      <img className="rounded-lg" src={thumbnails.medium.url} alt="thumbnail" />
-      <div className="flex my-2">
-        <img className="w-8 h-8" src={user} alt="user" />
-        <h1 className="font-bold mx-2 text-sm">
-          {title.length > 45 ? title.substr(0, 45) + "..." : title}
-        </h1>
+    <div className="w-64 h-64 hover:shadow-lg m-2 p-2 rounded-lg hover:animate-pulse">
+    <img className="rounded-lg" src={thumbnails.medium.url} alt="thumbnail" />
+    <div className="flex my-2">
+      <img className="w-8 h-8" src={user} alt="user" />
+      <div className="mx-2">
+        <div className="font-bold text-sm h-10">
+          {title.length > 40 ? title.substr(0, 40) + "..." : title}
+        </div>
+        <div className="flex mt-2">
+          <span className="text-xs">{channelTitle}</span>
+          <img className="w-3 h-3 mx-1" alt="verified" src={verified} />
+        </div>
+        <h2 className="text-xs mt-1">
+          {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+            statistics.viewCount
+          )}
+          {" Views • " + calculateDate(snippet.publishedAt)}
+        </h2>
       </div>
-      <div className="flex">
-        <span className="">{channelTitle}</span>
-        <img className="w-3 h-3 my-2 mx-1" alt="verified" src={verified} />
-      </div>
-      <h2 className="text-sm">
-        {Number(statistics.viewCount).toLocaleString("en-US")} Views •
-      </h2>
     </div>
-  );
+  </div>
+);
 };
 export default VideoCard;
